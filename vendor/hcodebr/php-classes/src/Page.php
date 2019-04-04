0b4ed3ed959f4d36b6db84bf38f7337f750ffcd3
Ttales R G S Silva <ttales@MacBook-Air-de-Ttales.local>
2019-04-03 23:36:19 -0300

    corrigindo página loja

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,6 +11,7 @@
             "data"=>[]
         ];
 
+        // Cria o head ao iniciar a instanciação da classe Page
         public function __construct($opts = array()){
 
             $this->options = array_merge($this->defaults, $opts);
@@ -30,17 +31,20 @@
 
         }
 
+        // Povoa um array com os dados da página
         private function setData($data = array()){
             foreach ($data as $key => $value) {
                 $this->tpl->assign($key, $value);
             }
         }
 
+        // Carrega o templeate
         public function setTpl($name, $data = array(), $returnHTML = false){
             $this->setData($data);
             return $this->tpl->draw($name, $returnHTML);
         }
 
+        // Cria o footer da página
         public function __destruct(){
 
             $this->tpl->draw("footer");
