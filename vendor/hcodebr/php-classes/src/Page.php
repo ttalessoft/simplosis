a03cfa1f2c5ddd15ee72b2387b30e90758644e9e
Ttales R G S Silva <ttales@MacBook-Air-de-Ttales.local>
2019-04-03 23:56:45 -0300

    configurando página admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -12,12 +12,12 @@
         ];
 
         // Cria o head ao iniciar a instanciação da classe Page
-        public function __construct($opts = array()){
+        public function __construct($opts = array(), $tpl_dir = "/views/"){
 
             $this->options = array_merge($this->defaults, $opts);
 
             $config = array(
-                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
                 "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
                 "debug"         => false
             );
