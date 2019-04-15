c4512c7dee44d316e4ccd814eb9c08273b700df4
Ttales R G S Silva <ttales@MacBook-Air-de-Ttales.local>
2019-04-15 11:53:23 -0300

    edit category of products

--- admin-categories.php
+++ admin-categories.php
@@ -1,7 +1,9 @@
 <?php
 
-use Hcode\Model\Category;
 use Hcode\Model\User;
+
+use Hcode\Model\Category;
+use Hcode\Model\Product;
 
 use Hcode\PageAdmin;
 
@@ -77,3 +79,53 @@ use Hcode\PageAdmin;
 		exit;
 
 	 });
+
+	 $app->get("/admin/categories/:idcategory/products", function($idcategory){
+
+		User::verifyLogin();
+
+		$category = new Category();
+		$category->get((int)$idcategory);
+
+		$page = new PageAdmin();
+		$page->setTpl("categories-products", [
+			'category'=>$category->getValues(),
+			'productsRelated'=>$category->getProducts(),
+			'productsNotRelated'=>$category->getProducts(false)
+		]);
+	 });
+
+
+	 $app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct){
+
+		User::verifyLogin();
+
+		$category = new Category();
+		$category->get((int)$idcategory);
+
+		$product = new Product();
+		$product->get((int)$idproduct);
+
+		$category->addProduct($product);
+
+		header("Location: /admin/categories/" . $idcategory . "/products");
+		exit;
+	 });
+
+	 $app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct){
+
+		User::verifyLogin();
+
+		$category = new Category();
+		$category->get((int)$idcategory);
+
+		$product = new Product();
+		$product->get((int)$idproduct);
+
+		$category->removeProduct($product);
+
+		header("Location: /admin/categories/" . $idcategory . "/products");
+		exit;
+	 });
+
+	 
--- site.php
+++ site.php
@@ -16,3 +16,15 @@ $app->get('/', function() {
 	));
 
 });
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>$category->getProducts()
+	]);
+});
